0a22fecd660b55856054ecb943946a80e60d13ee
Paweł Kubiak <pawel.kubiak@protonmail.com>
2021-01-22 11:20:23 +0100

    feat: Add metrics from flog tool

---
+++ metrics/flog.rb
@@ -0,0 +1,16 @@
+
+class MetricsFlog
+    def self.analyze(dirpath)
+        output = %x( bundle exec flog -a -c -s #{dirpath} )
+
+        output = output.split("\n").map { |line|
+            value, key = line.split(":")
+            [key.strip, value.strip.to_f]
+        }.to_h
+
+        return {
+            total: output["flog total"],
+            average_per_method: output["flog/method average"]
+        }
+    end
+end
